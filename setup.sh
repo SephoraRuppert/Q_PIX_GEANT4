46fb2ff826544aa706376fd5e6bb356278da3432
Johnny Ho <johnnyho@fas.harvard.edu>
2021-02-26 12:14:53 -0500

    load anaconda module

--- setup.sh
+++ setup.sh
@@ -10,4 +10,6 @@ module load root/6.14.00-fasrc01
 module load gcc/9.2.0-fasrc01 geant/4.10.06.p01-fasrc02
 source geant4.sh
 
+module load Anaconda/5.0.1-fasrc02
+
 cd /n/holystore01/LABS/guenette_lab/Users/jh/software/marley/build && source ../setup_marley.sh && cd /n/home04/sruppert/Q_PIX_GEANT4
